ed85a47723097d281d83bc4124abfb007e13af45
Priyanshu <priyanshuvsk09@gmail.com>
2024-07-03 20:58:02 +0530

    nothing

--- Conrollers/controller.js
+++ Conrollers/controller.js
@@ -6,7 +6,7 @@ const getAllTasks = async (req, res) => {
     res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ message: "Not User Found" });
-  } 
+  }
 };
 
 const createTasks = async (req, res) => {
@@ -18,16 +18,59 @@ const createTasks = async (req, res) => {
   }
 };
 
-const getTasks = (req, res) => {
-  res.send("Hello from getaltask");
+const getTasks = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Task.findById(id);
+    if (data) {
+      res.json(data);
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error Occured" });
+  }
 };
 
-const updateTasks = (req, res) => {
-  res.send("Hello from getaltask");
+const updateTasks = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const updatedData = await Task.findByIdAndUpdate(id, data, { new: true });
+    if (updatedData) {
+      res.json(updatedData);
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error Occured" });
+  }
+  // try {
+  //   const { id: taskId } = req.params;
+  //   const task = await Task.findByIdAndUpdate({ _id: taskId }, req.body);
+  //   if (!task) {
+  //     return res.status(404).json({ msg: `No task with id : ${taskId}` });
+  //   }
+  //   res.status(200).json(task);
+  // } catch (err) {
+  //   res.status(500).json({ msg: error });
+  // }
 };
 
-const deleteTasks = (req, res) => {
-  res.send("Hello from getaltask");
+const deleteTasks = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Task.findByIdAndDelete(id);
+    if (data) {
+      res.json({ message: "Data Deleted" });
+    } else {
+      res.status(404).json({ message: "Data Not Found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error Occured" });
+  }
 };
 
 module.exports = {
